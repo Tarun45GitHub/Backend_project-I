e2d8d385e4cec3e2854906c555d256c15ec5781f
Tarun45GitHub <td2545@it.jgec.ac.in>
2024-02-02 11:12:27 +0530

    api Error override

---
+++ src/utils/apiError.js
@@ -0,0 +1,19 @@
+class apiError extends Error{
+    constructor(
+        statusCode,
+        message="Some thing went wrong !",
+        errors=[],
+        stack=""
+    ){
+        super(message);
+        this.statusCode=statusCode;
+        this.message=message;
+        this.data=null;
+        this.errors=errors;
+        this.success=false;
+        if(stack) this.stack=stack;
+        else  Error.captureStackTrace(this,this.constructor);
+    }
+}
+
+export {apiError}
